7effc95b02324843c71f91aad64fb70d231789e9
jacek <zalewskii.jacek@gmail.com>
2023-12-31 00:48:46 +0100

    List of removed files in filter_recordings_287 added

--- birdclassification/preprocessing/filtering.py
+++ birdclassification/preprocessing/filtering.py
@@ -86,7 +86,13 @@ def filter_recordings_287(filepath_recordings='../data/xeno_canto_recordings.csv
     recordings_287: pandas.DataFrame
         Recordings of chosen 287 bird species
     """
-    recordings_to_remove = [818118]
+    recordings_to_remove = [470775, 798805, 684749, 644502, 357351, 809967, 624256, 809054, 807141, 162553, 817521,
+                            714657, 798808, 554614, 796603,798427, 257407, 257405, 787319, 791718, 507301, 80610,
+                            162683, 816529, 817344, 180914, 818891, 796288, 471258, 367690,818855, 818773, 600181,
+                            257406, 809965, 441473, 102788, 798809, 483205, 516953, 507302, 420376, 800153, 644503,
+                            802401, 798807, 562671, 357350, 806787, 162686, 798806, 600451, 397457, 567174, 791068,
+                            819062, 809966, 707378, 385433, 806174, 162612]
+
     df = pd.read_csv(filepath_bird_list, delimiter=";")
     species_287 = df[df["Chosen"] == on_list]
     print(len(species_287.index))
